d5413cc4e5c118b392ac96b226a751c56e2cf4ff
Unixer <unixer@tiscali.it>
2013-07-04 23:06:41 +0200

    Update to kernel 3.10

--- packages/busybox/profile.d/system.sh
+++ packages/busybox/profile.d/system.sh
@@ -1,6 +1,6 @@
 # System Settings
 
-export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin"
+export PATH="/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/lib/bluetooth"
 export HOSTNAME=$(cat /etc/hostname)
 PS1='\[\e[0;32m\]\u\[\e[m\] \[\e[1;34m\]\w\[\e[m\] \[\e[1;32m\]\$\[\e[m\] \[\e[1;37m\]'
 export PS1
@@ -12,6 +12,7 @@ export TERM
 
 alias ll='ls -al'
 alias l='ls'
+alias dir='ls -l'
 alias stop="systemctl stop"
 alias start="systemctl start"
 alias restart="systemctl restart"
