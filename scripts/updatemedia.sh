2c2d1de662cbf2cfb8313ca82242075952be9ec9
Unixer <unixer@tiscali.it>
2013-08-05 21:30:39 +0200

    Update to 1.2

--- scripts/updatemedia.sh
+++ scripts/updatemedia.sh
@@ -2,27 +2,28 @@
 CWD=$(pwd)
 dest=$CWD/linux-000.06-media-update.patch
 cd /tmp
-wget http://linuxtv.org/downloads/drivers/linux-media-2013-02-09.tar.bz2
+#wget http://linuxtv.org/downloads/drivers/linux-media-2013-02-09.tar.bz2
+wget http://linuxtv.org/downloads/drivers/linux-media-2013-06-29.tar.bz2
 wget http://linuxtv.org/downloads/drivers/linux-media-LATEST.tar.bz2
-mkdir linux-media-2013-02-09
+mkdir linux-media-2013-06-29
 mkdir linux-media-LATEST
-cd linux-media-2013-02-09
-tar jxvf ../linux-media-2013-02-09.tar.bz2
-rm drivers/media/rc/lirc_dev.c
-rm drivers/media/v4l2-core/v4l2-dev.c
-rm git_log
-rm kernel_version.h
+cd linux-media-2013-06-29
+tar jxvf ../linux-media-2013-06-29.tar.bz2
+#rm drivers/media/rc/lirc_dev.c
+#rm drivers/media/v4l2-core/v4l2-dev.c
+#rm git_log
+#rm kernel_version.h
 cd ..
 cd linux-media-LATEST
 tar jxvf ../linux-media-LATEST.tar.bz2
-rm drivers/media/rc/lirc_dev.c
-rm drivers/media/v4l2-core/v4l2-dev.c
-rm git_log
-rm kernel_version.h
+#rm drivers/media/rc/lirc_dev.c
+#rm drivers/media/v4l2-core/v4l2-dev.c
+#rm git_log
+#rm kernel_version.h
 cd ..
-diff -urN linux-media-2013-02-09 linux-media-LATEST > $dest
-rm linux-media-2013-02-09.tar.bz2 || true
+diff -urN linux-media-2013-06-29 linux-media-LATEST > $dest
+rm linux-media-2013-06-29.tar.bz2 || true
 rm linux-media-LATEST.tar.bz2 || true
-rm -rf linux-media-2013-02-09 || true
+rm -rf linux-media-2013-06-29 || true
 rm -rf linux-media-LATEST || true
 
