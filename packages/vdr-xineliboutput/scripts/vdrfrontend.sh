4ae3eccf592b0d92ec6e60e9960fce3d2848d00e
Unixer <unixer@tiscali.it>
2013-11-15 18:57:09 +0100

    Update for ATI-Radeon

--- packages/vdr-xineliboutput/scripts/vdrfrontend.sh
+++ packages/vdr-xineliboutput/scripts/vdrfrontend.sh
@@ -8,6 +8,11 @@ fi
 if lsmod | grep gma500_gfx > /dev/null; then
    DRIVER="--video=xshm"
 fi
+if lsmod | grep radeon > /dev/null; then
+   DRIVER="--video=vaapi"
+   export LIBVA_DRIVER_NAME=vdpau
+   export VDPAU_DRIVER=r600
+fi
 HUDOPTS=""
 XINELIBOUTPUTOPTS="$DRIVER --fullscreen --aspect=16:9 --reconnect --post tvtime:method=use_vo_driver --audio=alsa --syslog --silent --tcp --hud=xshape"
 CONFIG="--config /etc/vdr-sxfe/config_xineliboutput"
@@ -15,5 +20,6 @@ CONFIG="--config /etc/vdr-sxfe/config_xineliboutput"
 while  ! netcat -z localhost 37890; do sleep 0.1; done;
 #while ! grep -q "^ 1" /proc/asound/cards ; do sleep 1 ; done
 #while ! grep -q "^ 0" /proc/asound/cards ; do sleep 1 ; done
+sleep 3
 /usr/bin/vdr-sxfe $XINELIBOUTPUTOPTS $CONFIG xvdr://127.0.0.1:37890 &> /tmp/vdr-frontend.log 
 
