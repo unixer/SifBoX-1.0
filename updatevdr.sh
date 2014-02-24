130751ab54021b7fb416ea241928bf4d95f6bc93
Unixer <unixer@tiscali.it>
2014-02-24 15:16:13 +0100

    Update

--- updatevdr.sh
+++ updatevdr.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-VDRDIR="`ls -d packages/vdr*`"
+. packages/vdr/meta
+VDR_REV=$PKG_REV
+
+VDRDIR="`ls -d packages/vdr-*`"
 for dir in $VDRDIR; do
-  DIR="`ls -d packages/vdr*/meta | tr ' ' '\n'`" 
-  sed -i "s|PKG_REV=$1|PKG_REV=$2|" $DIR
+  DIR="`ls -d packages/vdr-*/meta | tr ' ' '\n'`" 
+  . $dir/meta
+  PLG_REV=$PKG_REV
+  echo $PLG_REV $VDR_REV $PKG_REV
+  sed -i "s|PKG_REV=$PLG_REV|PKG_REV=$VDR_REV|" $DIR
 done
